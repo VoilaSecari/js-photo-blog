915bc1f76985b99075453a462e1d0cc678a5ec8b
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-01 21:50:59 +0200

    chiamata tutte le immagini

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 //CHIAMATA IMMAGINE
 
-axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
-  let immagine = response.data[0].url;
-  document.getElementById("foto-1").src = immagine;
-});
+// axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
+//   const immagine = response.data[0].url;
+//   document.getElementById("foto-1").src = immagine;
+// });
+
+axios
+  .get("https://lanciweb.github.io/demo/api/pictures/")
+  .then((response) => {
+    const immagini = response.data;
+    for (let i = 0; i < 6; i++) {
+      const imgEl = document.getElementById(`foto-${i + 1}`);
+      if (imgEl && immagini[i]) {
+        imgEl.src = immagini[i].url;
+      }
+    }
+  })
+  .catch((error) => {
+    console.error(
+      "Errore ricontrolla, la richiesta non è andata a buon fine :(",
+      error
+    );
+  });
