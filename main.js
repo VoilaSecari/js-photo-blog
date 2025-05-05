2f7248b1adf44883ede52284f17143aaefc1a26e
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-05 18:18:43 +0200

    modificato js per chiamare card interamente da js, da aggiungere overlay e click

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
 //CHIAMATA IMMAGINE
 
-// axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
-//   const immagine = response.data[0].url;
-//   document.getElementById("foto-1").src = immagine;
-// });
+//Qui avevo provato a lasciare tutte le card in pagina e sostituire gli elementi anziché crearla direttamente.
 
-axios
-  .get("https://lanciweb.github.io/demo/api/pictures/")
-  .then((response) => {
-    const immagini = response.data;
-    for (let i = 0; i < 6; i++) {
-      const imgEl = document.getElementById(`foto-${i + 1}`);
-      if (imgEl && immagini[i]) {
-        imgEl.src = immagini[i].url;
-      }
-    }
-  })
-  .catch((error) => {
-    console.error(
-      "Errore ricontrolla, la richiesta non è andata a buon fine :(",
-      error
-    );
+// axios
+//   .get("https://lanciweb.github.io/demo/api/pictures/")
+//   .then((response) => {
+//     const immagini = response.data;
+//     for (let i = 0; i < 6; i++) {
+//       const imgEl = document.getElementById(`foto-${i + 1}`);
+//       if (imgEl && immagini[i]) {
+//         imgEl.src = immagini[i].url;
+//       }
+//     }
+//   })
+//   .catch((error) => {
+//     console.error(
+//       "Errore ricontrolla, la richiesta non è andata a buon fine :(",
+//       error
+//     );
+//   });
+
+// IMPOSTARE ELEMENTO CONTAINER
+const postsContainerEl = document.getElementById("posts-container");
+
+// NUOVA CHIAMATA, CREANDO POST CON JS E BASTA
+
+axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
+  const posts = response.data;
+  // console.log(posts);
+
+  //POST SONO CORRETTAMENTE STATI RICEVUTI
+
+  //CREAZIONE OGNI POST
+  let cardHTML = "";
+  posts.forEach((post) => {
+    cardHTML += generatePostCard(post);
   });
+
+  postsContainerEl.innerHTML += cardHTML;
+});
+
+// FUNZIONE ---> sostituzione dati post in HTML
+
+const generatePostCard = (post) => {
+  const cardHTML = `
+  <div class="card">
+          <img src="./IMG/pin.svg" alt="Pin" class="card-pin" />
+          <div class="card-content">
+            <img src="${post.url}" alt="${post.title}" />
+          </div>
+          <div class="card-content">
+            <div class="card-title">
+              <p>
+                ${post.title}
+              </p>
+            </div>
+            <div class="card-date">
+              <p id="card-date-text">
+                ${post.date}
+              </p>
+            </div>
+          </div>
+        </div>
+  `;
+
+  // RITORNO funzione arrow
+
+  return cardHTML;
+};
